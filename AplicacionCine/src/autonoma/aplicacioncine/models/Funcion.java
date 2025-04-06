10894d29d69732c7536836ad1a9eb2a11876a0f2
Kallst <santiago.castrom@autonoma.edu.co>
2025-04-06 16:59:10 -0500

    Se llenó la clase abstracta Funcion

--- AplicacionCine/src/autonoma/aplicacioncine/models/Funcion.java
+++ AplicacionCine/src/autonoma/aplicacioncine/models/Funcion.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package autonoma.aplicacioncine.models;
 
 /**
  *
  * @author LAPTOP
  */
-public class Funcion {
+
+public abstract class Funcion {
+    
+    protected String horario;
+
+    public Funcion(String horario) {
+        this.horario = horario;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public abstract double getDescuento();  // Retorna un valor entre 0.0 y 1.0
+    public abstract String getNombre();     // Retorna el nombre de la función
     
 }
