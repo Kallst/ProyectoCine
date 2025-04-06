574e6d0bf600dd57b7f5c9cac0742c5c2caef427
Kallst <santiago.castrom@autonoma.edu.co>
2025-04-06 16:48:36 -0500

    Se crea la clase UsuarioNino que implementa la interfaz Usuario

---
+++ AplicacionCine/src/autonoma/aplicacioncine/models/UsuarioNino.java
@@ -0,0 +1,20 @@
+package autonoma.aplicacioncine.models;
+
+/**
+ *
+ * @author LAPTOP
+ */
+public class UsuarioNino implements IUsuario 
+{
+
+    @Override
+    public int getDescuento() {
+        return 500;
+    }
+
+    @Override
+    public String getTipo() {
+        return "Niño";
+    }
+    
+}
