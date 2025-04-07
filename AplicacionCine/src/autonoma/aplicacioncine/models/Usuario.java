a4e1ee02094a47bcfc94a66a6550139e289c8fc4
Kallst <santiago.castrom@autonoma.edu.co>
2025-04-06 20:08:37 -0500

    Se añaderon al proyecto las imágenes que vamos a usar para la interfaz gráfica y creamos la interfaz VentanaPrincipal para manejar los procesos

--- AplicacionCine/src/autonoma/aplicacioncine/models/Usuario.java
+++ AplicacionCine/src/autonoma/aplicacioncine/models/Usuario.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package autonoma.aplicacioncine.models;
 
 /**
  *
  * @author LAPTOP
  */
-public class Usuario {
+public abstract class Usuario {
+    protected String nombre;
+
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public abstract int getDescuento();
+    public abstract String getTipo();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
 }
